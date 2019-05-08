5a29875cdb1cc793366e1ab65e6c3f01c692f729
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2019-05-08 14:07:34 -0400

    initial implementation

---
+++ grammar.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import tatsu    # parser generator library
+import pprint   # pretty-printer
+import reprlib
+from semantics import Semantics
+
+class Grammar():
+
+    def __init__(self, semantics=Semantics()):
+        self.semantics = semantics
+
+    grammar = r'''
+@@grammar::EVAL
+
+start = expression $ ;
+
+expression = left:expression op:'+' right:term
+           | left:expression op:'-' right:term
+           | term
+;
+
+term =
+         left:term op:'*' right:factor
+       | left:term op:'×' right:factor
+       | left:term op:'/' right:factor
+       | left:term op:'÷' right:factor
+       | factor
+;
+
+factor =
+      left:base '**' right:factor
+    | left:base '^' right:factor
+    | base;
+
+base = number | compound_expression;
+
+compound_expression = '(' expression ')' ;
+
+number = /\d+/ | pi;
+pi = 'π' | 'pi';
+
+var = /[A-Za-z]\w*/ ;
+
+'''
+
+    parser = tatsu.compile(grammar, name="eval")
+
+    def parse(self, expr):
+        return self.parser.parse(expr, semantics=self.semantics)
---
+++ semantics.py
@@ -0,0 +1,58 @@
+
+import os
+from tatsu.ast import AST
+from pprint import pprint
+
+def s(x): str(x)
+
+if int(os.environ.get('DEBUG', 0)):
+  def debug(*strings):
+    print("".join(map(str, strings)))
+else:
+  def debug(*strings):
+    pass
+
+class Semantics:
+  def expression(self, ast):
+    debug("** expression: ", ast)
+    if isinstance(ast, AST):
+      if ast.op == '+':
+        return ast.left + ast.right
+      elif ast.op == '-':
+        return ast.left - ast.right
+      else:
+        raise Exception("WTF", ast.op)
+    else:
+      return ast
+
+  def term(self, ast):
+    debug("** term: ", ast)
+    if isinstance(ast, AST):
+      if ast.op == '*' or ast.op == '×':
+        return ast.left * ast.right
+      elif ast.op == '/' or ast.op == '÷':
+        return ast.left / ast.right
+      else:
+        raise Exception("WTF", ast.op)
+    else:
+      return ast
+
+  def compound_expression(self, ast):
+    return ast[1]
+
+  def factor(self, ast):
+    if isinstance(ast, AST):
+      return ast.left ** ast.right
+    else:
+      return ast
+
+  def number(self, ast):
+    return float(ast)
+
+  def pi(self, ast):
+    return 3.14159
+
+  
+        
+
+  
